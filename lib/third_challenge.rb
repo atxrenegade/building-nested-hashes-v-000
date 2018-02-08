97bc841bc69282cdee90a3327c828b2e1b75287f
Harleigh Abel <harleighabel@gmail.com>
2018-02-08 05:51:46 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,8 +2,8 @@ def third_challenge
   epic_tragedy = {
     montague: {
       patriarch: {
-        name: "Lord Montague"
-        age: 50
+        name: "Lord Montague",
+        age: 53
       }
     }
   }
